5bd062020438ad115e6ca46ccd160ed8bc7cec3f
okbWork <okb3@njit.edu>
2021-10-26 19:28:32 -0400

    fixed lint errors

--- calculator/main.py
+++ calculator/main.py
@@ -11,22 +11,18 @@ class Calculator:
     def add_number(self, value_a):
         """ adds number to result"""
         self.result = self.result + value_a
-        return self.result
 
     def subtract_number(self, value_a):
         """ subtract number from result"""
         self.result = self.result - value_a
-        return self.result
 
     def multiply_numbers(self, value_a, value_b):
         """ multiply two numbers and get result"""
         self.result = value_a * value_b
-        return self.result
 
     def divide_numbers(self, value_a, value_b):
         """With two real values, divide value a by value b (a / b)"""
         try:
             self.result = value_a / value_b
-            return self.result
-        except ZeroDivisionError:
-            raise ZeroDivisionError("ZeroDivisionError: integer division or modulo by zero")
+        except Exception as e:
+            raise ZeroDivisionError
